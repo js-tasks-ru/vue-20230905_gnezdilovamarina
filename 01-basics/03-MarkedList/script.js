92fa246c72f5e77b9266ea309865fde79a8a08e8
Marina Gnezdilova <gnezdilovamarina@yandex.ru>
2023-09-10 18:01:35 +0300

    MarkedList

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // From https://jsonplaceholder.typicode.com/comments
 const emails = [
@@ -30,3 +30,29 @@ const emails = [
 ];
 
 // Требуется создать Vue приложение
+
+const App = defineComponent({ 
+  name: 'App',
+  data() {
+    return {
+      emails: emails,
+      search: ' '
+    }
+  },
+  computed: {
+    newEmails() {
+      const markedEmail = (email) => {
+        const newEmail = {};
+        newEmail.email = email;
+        newEmail.isMarked = email.toLowerCase().includes(this.search.toLowerCase())
+        return newEmail
+      }
+      const markedEmails = this.emails.map((email) => markedEmail(email));
+        return markedEmails
+    },
+  },
+});
+const app = createApp(App);
+const vm = app.mount('#app');
+window.vm = vm;
+console.log(vm);
