0572aee5964359977c6d97095fc6cf5f88163a4f
Marina Gnezdilova <gnezdilovamarina@yandex.ru>
2023-09-10 15:58:28 +0300

    VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,41 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { first } from 'lodash';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+const App = defineComponent({
+  name: 'App',
+  data() {
+    return {
+      firstOperand: null,
+      secondOperand: null,
+      operator: '',
+    };
+  },
+  computed: {
+    result() {
+      const res = () => {
+        if (this.operator === 'sum') {
+          return +this.firstOperand + +this.secondOperand;
+        }
+        if (this.operator === 'subtract') {
+          return this.firstOperand - this.secondOperand;
+        }
+        if (this.operator === 'multiply') {
+          return this.firstOperand * this.secondOperand;
+        }
+        if (this.operator) {
+          return this.firstOperand / this.secondOperand;
+        } else {
+          return ' ';
+        }
+      };
+      return res;
+    },
+  },
+});
+
+const app = createApp(App);
+const vm = app.mount('#app');
+
+window.vm = vm;
